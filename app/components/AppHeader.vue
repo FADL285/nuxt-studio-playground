8e7b7bc61a4d60451ab89af91cd6a7580dcc08dd
Mohamed Fadl <mohamed@fadl.info>
2026-02-27 04:19:28 +0200

    Remove TemplateMenu component and update AppHeader to reflect changes. Modify index.yml to update links for Nuxt Studio and Nuxt Content.

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -31,8 +31,6 @@ nuxtApp.hooks.hookOnce('page:finish', () => {
       <NuxtLink to="/">
         <AppLogo class="w-auto h-6 shrink-0" />
       </NuxtLink>
-
-      <TemplateMenu />
     </template>
 
     <template #right>
